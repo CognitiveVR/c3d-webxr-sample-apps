1c9311d99d060265ef69935bf951425ec0579bb5
gtamb2 <gillian@cognitive3d.com>
2025-06-04 11:53:29 -0700

    Uploaded Basic Demo
    The first demo app that will be used to test basic controller functions for the WebXR SDK.

---
+++ Basic Demo/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2251374.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 232167664, 232167659, 232167674, 232167666, 232167663, 232167672, 232167673, 232181345, 233020288, 233083047, 233084274, 233085221 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/232177594/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/232177595/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/232177593/1/ammo.js', 'preload' : true},
+];
