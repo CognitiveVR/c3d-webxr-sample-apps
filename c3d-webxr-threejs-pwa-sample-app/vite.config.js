8c92a3e3dda44a9a56daf2936cff5001f90faed9
Ali Raza Syed <a.raza.syed.123@gmail.com>
2025-09-18 14:18:07 -0400

    Changes to vite-config in threejs sample app to handle local dependencies (webxr sdk)

--- c3d-webxr-threejs-pwa-sample-app/vite.config.js
+++ c3d-webxr-threejs-pwa-sample-app/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default defineConfig({
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate', 
+      registerType: 'autoUpdate',
       manifest: {
-        name: 'Cog3D WebXR - ThreeJS VR Project with PWA support', 
-        short_name: 'ThreeJS VR', 
-        start_url: '/index.html', 
+        name: 'Cog3D WebXR - ThreeJS VR Project with PWA support',
+        short_name: 'ThreeJS VR',
+        start_url: '/index.html',
         display: 'standalone',
         background_color: '#505050',
         theme_color: '#505050',
@@ -22,9 +28,14 @@ export default defineConfig({
           }
         ]
       },
-      srcDir: 'public', 
-      filename: 'service-worker.js', 
-      strategies: 'injectManifest', 
+      srcDir: 'public',
+      filename: 'service-worker.js',
+      strategies: 'injectManifest',
     })
   ],
+  resolve: {
+    alias: {
+      'three': path.resolve(__dirname, 'node_modules/three'),
+    }
+  }
 });
