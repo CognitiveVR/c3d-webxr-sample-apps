605acbf55ee032ce79003a61ab7d8156de3139de
Ali Raza Syed <a.raza.syed.123@gmail.com>
2025-10-03 16:13:40 -0400

    Added scaling feature of objects for dynamic object snapshots

--- c3d-webxr-threejs-pwa-sample-app/src/cognitive.js
+++ c3d-webxr-threejs-pwa-sample-app/src/cognitive.js
@@ -4,6 +4,35 @@ import C3DThreeAdapter from '@cognitive3d/analytics/adapters/threejs';
 export let c3d;
 let c3dAdapter;
 
+// export function initializeC3D(renderer) {
+//     if (c3d) return c3d; 
+
+//     c3d = new C3DAnalytics({
+//         config: {
+//             APIKey: import.meta.env.VITE_C3D_APPLICATION_KEY,
+//             allSceneData: [{
+//                 sceneName: "SampleScene",
+//                 sceneId: "50542ff3-4f51-4c9a-99d8-6082921953f9",
+//                 versionNumber: "1"
+//             }]
+//         }
+//     }, renderer); 
+
+//     c3d.setScene('SampleScene');
+//     c3d.userId = 'threejs_user_' + Date.now();
+//     c3d.setUserName('aligator');
+//     c3d.setDeviceProperty("AppName", "ThreeJS_WebXR_SDK_Test_App");
+//     c3d.setUserProperty("c3d.app.version", "0.2");
+//     c3d.setUserProperty("c3d.deviceid", 'threejs_windows_device_' + Date.now());
+
+
+
+//     c3d.setParticipantFullName('Ali Raza');  // Replaced setUserName
+
+//     c3dAdapter = new C3DThreeAdapter(c3d);
+    
+//     return c3d;
+// }
 export function initializeC3D(renderer) {
     if (c3d) return c3d; 
 
@@ -19,12 +48,22 @@ export function initializeC3D(renderer) {
     }, renderer); 
 
     c3d.setScene('SampleScene');
-    c3d.userId = 'threejs_user_' + Date.now();
-    c3d.setUserName('ThreeJS_SDK_Test_User');
+    
+    //  PARTICIPANT SETUP 
+    c3d.setParticipantFullName('ThreeJS_SDK_Test_User');  
+    c3d.setParticipantId('participant_' + Date.now());   
+    
+    //  DEVICE PROPERTIES 
     c3d.setDeviceName('WindowsPCBrowserVR');
-    c3d.setDeviceProperty("AppName", "ThreeJS_WebXR_SDK_Test_App");
-    c3d.setUserProperty("c3d.app.version", "0.2");
+    c3d.setDeviceProperty("AppName", "c3d-webxr-threejs-pwa-sample-app");
+    
+    //  USER PROPERTIES 
+    c3d.setUserProperty("c3d.app.version", "0.5");
     c3d.setUserProperty("c3d.deviceid", 'threejs_windows_device_' + Date.now());
+    
+    
+    //  SESSION PROPERTIES  
+    c3d.setSessionProperty("platform", "webxr");
 
     c3dAdapter = new C3DThreeAdapter(c3d);
     
